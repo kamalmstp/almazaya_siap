f79be2483eb24dab047ec207e1805a484ba6ce7d
Halimah <32287174+halimahdiah@users.noreply.github.com>
2020-09-07 11:31:33 +0800

    tambah backend sedikit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Admin extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,8 +20,29 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->vars['halaman'] = 'frontend';
-		$this->vars['content'] = 'welcome_message';
+		$this->vars['halaman'] = 'dashboard';
+		$this->vars['content'] = 'admin/dashboard';
+		$this->load->view('index', $this->vars);
+	}
+
+	public function pengaduan()
+	{
+		$this->vars['halaman'] = 'pengaduan';
+		$this->vars['content'] = 'admin/pengaduan';
+		$this->load->view('index', $this->vars);
+	}
+	
+	public function aspirasi()
+	{
+		$this->vars['halaman'] = 'aspirasi';
+		$this->vars['content'] = 'admin/aspirasi';
+		$this->load->view('index', $this->vars);
+	}
+	
+	public function pengumuman()
+	{
+		$this->vars['halaman'] = 'pengumuman';
+		$this->vars['content'] = 'admin/pengumuman';
 		$this->load->view('index', $this->vars);
 	}
 }
